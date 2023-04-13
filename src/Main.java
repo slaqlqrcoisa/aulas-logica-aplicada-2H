2643061fa4c3eb7180bbc2edf5b206a1d2477ed3
programadormovel <programadormovel@gmail.com>
2023-04-13 08:17:37 -0300

    Criado laço while e for

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int contador = 0;
+        // laço de repetição
+        while(contador < 10){
+            System.out.println("Contador = " + contador);
+            // incremento
+            contador = contador + 1;
+        }
+
+        // laço para faça
+        for(int x=0; x < 10; x++){
+            System.out.println("Contador = " + x);
+        }
+
     }
 }
